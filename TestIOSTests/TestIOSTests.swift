7f5c7f60dd961cc4d908230db14566e58b4f7db3
Marcel Braghetto <marcel.braghetto@area360.com>
2015-11-19 15:09:13 +1300

    Test app.

---
+++ TestIOSTests/TestIOSTests.swift
@@ -0,0 +1,37 @@
+//
+//  TestIOSTests.swift
+//  TestIOSTests
+//
+//  Created by STQRY12 on 18/11/15.
+//  Copyright (c) 2015 Test. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class TestIOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+//        XCTAssert(true == false, "Bad match")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
