0b18ceb966bd046ac67ec6bdb6adc85067f76039
Marcel Braghetto <marcel.braghetto@area360.com>
2015-11-20 09:34:13 +1300

    Adjusting report format

--- run_tests.sh
+++ run_tests.sh
@@ -4,4 +4,4 @@ echo "Installing pods..."
 pod install
 
 echo "Running tests..."
-xcodebuild test -project TestIOS.xcodeproj -scheme TestIOS -destination 'platform=iOS Simulator,name=iPhone 4s,OS=9.1' -enableCodeCoverage YES | xcpretty -c --report xml
+xcodebuild test -project TestIOS.xcodeproj -scheme TestIOS -destination 'platform=iOS Simulator,name=iPhone 4s,OS=9.1' -enableCodeCoverage YES | xcpretty -c --report junit
