624915f1931e9fe545e8d4445ff5a4c7341c58ca
Marcel Braghetto <marcel.braghetto@area360.com>
2015-11-20 08:55:58 +1300

    Adding script

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
+#!/bin/bash -l
 # Run Tests
 echo "Running tests..."
 xcodebuild test -project TestIOS.xcodeproj -scheme TestIOS -destination 'platform=iOS Simulator,name=iPhone 4s,OS=8.4' -enableCodeCoverage YES | xcpretty -c --report html
