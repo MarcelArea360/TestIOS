7d4a510ab71039ca1e2c49196ff02268f8f977d8
Marcel Braghetto <lilarcordev@gmail.com>
2015-11-20 05:57:37 +1300

    Added podfile

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,6 @@
 # Run Tests
+echo "Installing pods..."
+pod install
+
 echo "Running tests..."
 xcodebuild test -project TestIOS.xcodeproj -scheme TestIOS -destination 'platform=iOS Simulator,name=iPhone 4s,OS=9.1' -enableCodeCoverage YES | xcpretty -c --report xml
